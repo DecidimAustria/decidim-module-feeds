f95ee64d032aa1cc9c28ecd05b644ced5bd1600a
webchroma <info@webchroma.de>
2024-04-19 13:20:23 +0000

    content_block implementiert / anpassung feed layout an decidim

---
+++ app/cells/decidim/feeds/content_blocks/feeds_settings_form_cell.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Feeds
+    module ContentBlocks
+      class FeedsSettingsFormCell < Decidim::ViewModel
+        alias form model
+      end
+    end
+  end
+end
--- lib/decidim/feeds/engine.rb
+++ lib/decidim/feeds/engine.rb
@@ -3,21 +3,44 @@
 require "rails"
 require "decidim/core"
 
+require_relative "content_blocks/content_blocks_homepage"
+
 module Decidim
   module Feeds
     # This is the engine that runs on the public interface of feeds.
     class Engine < ::Rails::Engine
+      
       isolate_namespace Decidim::Feeds
 
-      routes do
-        # Add engine routes here
-        # resources :feeds
-        # root to: "feeds#index"
+      # initializer "decidim_feeds.overrides", after: "decidim.action_controller" do
+      #   config.to_prepare do
+      #     Decidim::HomepageController.include(Decidim::Feeds::NeedsContentBlocksSnippets)
+      #   end
+      # end
+
+      initializer "decidim_feeds.snippets" do |app|
+        app.config.enable_html_header_snippets = true
       end
 
-      initializer "Feeds.webpacker.assets_path" do
+      initializer "decidim_feeds.add_cells_view_paths" do
+        Cell::ViewModel.view_paths << File.expand_path("#{Decidim::Feeds::Engine.root}/app/cells")
+        Cell::ViewModel.view_paths << File.expand_path("#{Decidim::Feeds::Engine.root}/app/views")
+      end
+
+      initializer "decidim_feeds.webpacker.assets_path" do
         Decidim.register_assets_path File.expand_path("app/packs", root)
       end
+
+      initialize_homepage_content_blocks
+
+      initializer "decidim_feeds.register_icons" do
+        Decidim.icons.register(name: "function-line", icon: "function-line", category: "system", description: "", engine: :feeds)
+        Decidim.icons.register(name: "home-5-line", icon: "home-5-line", category: "system", description: "", engine: :feeds)
+        Decidim.icons.register(name: "recycle-line", icon: "recycle-line", category: "system", description: "", engine: :feeds)
+        Decidim.icons.register(name: "shake-hands-line", icon: "shake-hands-line", category: "system", description: "", engine: :feeds)
+        Decidim.icons.register(name: "gallery-fill", icon: "gallery-fill", category: "system", description: "", engine: :feeds)
+      end
+
     end
   end
 end
