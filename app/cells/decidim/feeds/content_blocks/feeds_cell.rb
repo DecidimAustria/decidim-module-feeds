a74c9cf945c64f5f353032e77f88b35b2f88e439
webchroma <info@webchroma.de>
2024-05-17 11:05:24 +0000

    splitted single elements / layout for single cell

--- app/cells/decidim/feeds/content_blocks/feeds_cell.rb
+++ app/cells/decidim/feeds/content_blocks/feeds_cell.rb
@@ -4,7 +4,19 @@ module Decidim
   module Feeds
     module ContentBlocks
       class FeedsCell < Decidim::ViewModel
-        include Partial
+        include Cell::ViewModel::Partial
+
+        def all_categories
+          return if @all_categories.present?
+
+          @category_ids ||= Decidim::Category.pluck(:id, :decidim_participatory_space_type, :decidim_participatory_space_id).select do |category|
+            _id, space_type, space_id = category
+            space = space_type.constantize.find(space_id)
+            space.organization == current_organization
+          end.map(&:first)
+
+          @all_categories ||= Decidim::Category.where(id: @category_ids)
+        end
       end
     end
   end
