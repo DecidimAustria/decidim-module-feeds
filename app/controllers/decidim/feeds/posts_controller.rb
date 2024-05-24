e3822632111c5cfec82b549f761350b70d4d267f
webchroma <info@webchroma.de>
2024-05-24 11:53:14 +0000

    new feeds

--- app/controllers/decidim/feeds/posts_controller.rb
+++ app/controllers/decidim/feeds/posts_controller.rb
@@ -3,6 +3,11 @@ require_dependency "decidim/feeds/application_controller"
 module Decidim::Feeds
   class PostsController < ApplicationController
     def index
+      @form = Decidim::Feeds::Post.new
+    end
+
+    def new
+      @form = Decidim::Feeds::Post.new
     end
   end
 end
--- app/packs/src/decidim/feeds/feeds.js
+++ app/packs/src/decidim/feeds/feeds.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 	console.log('feed JS really loaded');
+
 	const newFeed = document.querySelector('.feeds__feed_newElement');
 	const newFeedOpener = document.querySelector(
 		'.feeds__feed_newElement-opener'
@@ -10,4 +11,27 @@ document.addEventListener('DOMContentLoaded', function () {
 		newFeedOpener.setAttribute('aria-expanded', !isExpanded);
 		newFeed.classList.toggle('open');
 	});
+
+	const newFeedForm = document.querySelector('.feeds__feed_newElement form');
+	const newFeedFormPostType = newFeedForm.querySelectorAll(
+		'.postType input[type="radio"]'
+	);
+
+	const newFeedCalendarForm = document.getElementById('extraFieldsForCalendar');
+	const newFeedSurveyForm = document.getElementById('extraFieldsForSurvey');
+
+	newFeedFormPostType.forEach(function (radio) {
+    radio.addEventListener('change', function () {
+        // Hide all forms
+        newFeedCalendarForm.classList.remove('open');
+        newFeedSurveyForm.classList.remove('open');
+
+        // Display the extra fields for the selected radio button
+        if (this.value === 'calendar') {
+            newFeedCalendarForm.classList.add('open');
+        } else if (this.value === 'survey') {
+            newFeedSurveyForm.classList.add('open');
+        }
+    });
+});
 });
