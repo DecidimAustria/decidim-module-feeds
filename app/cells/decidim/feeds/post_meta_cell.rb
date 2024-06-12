99922930dde91ae00961d4f3c731b8affa9a43d0
webchroma <info@webchroma.de>
2024-06-12 11:17:26 +0000

    comments

--- app/cells/decidim/feeds/post_meta_cell.rb
+++ app/cells/decidim/feeds/post_meta_cell.rb
@@ -4,14 +4,6 @@ module Decidim
   module Feeds
     class PostMetaCell < Decidim::ViewModel
       include Cell::ViewModel::Partial
-      include Decidim::TooltipHelper
-      include Decidim::CardHelper
-      include Decidim::LayoutHelper
-      include Decidim::SearchesHelper
-      include ::Decidim::EndorsableHelper
-      include ::Decidim::FollowableHelper
-
-      alias resource model
 
       def show
         render :show
