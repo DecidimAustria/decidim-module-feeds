ec992ba6ecb13727ed21b65e4f0a64301e3ce241
webchroma <info@webchroma.de>
2024-06-20 09:16:35 +0000

    meta cell

--- app/cells/decidim/feeds/post_meta_cell.rb
+++ app/cells/decidim/feeds/post_meta_cell.rb
@@ -4,6 +4,11 @@ module Decidim
   module Feeds
     class PostMetaCell < Decidim::ViewModel
       include Cell::ViewModel::Partial
+      include ::Devise::Controllers::Helpers
+      include ::Devise::Controllers::UrlHelpers
+      include Messaging::ConversationHelper
+      include Decidim::FollowableHelper
+      include ERB::Util
 
       def show
         render :show
@@ -12,6 +17,12 @@ module Decidim
       def post
         model
       end
+
+      property :profile_path
+      property :can_be_contacted?
+      property :has_tooltip?
+
+      delegate :current_user, to: :controller, prefix: false
       
     end
   end
