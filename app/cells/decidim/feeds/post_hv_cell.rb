49f3a81f51a095983155d1d6699c24a42266c273
webchroma <info@webchroma.de>
2024-07-03 12:46:32 +0000

    HV post logik

--- app/cells/decidim/feeds/post_hv_cell.rb
+++ app/cells/decidim/feeds/post_hv_cell.rb
@@ -20,6 +20,30 @@ module Decidim
         model.category
       end
 
+      # post status / for HV post
+      # nil default
+      # 1 bearbeitung
+      # 2 erledigt
+
+      def post_status
+        nil
+      end
+
+      def status_class
+        case post_status
+          when 1
+            'warning'
+          when 2
+            'success'
+          else
+            ''
+          end
+      end
+
+      def post_highlighted
+        false
+      end
+
       def post_commentable
         model.enable_comments?
       end
