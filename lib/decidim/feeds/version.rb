6d594ba9a8435a3bb848de7ac6a0822390a6af0f
Alexander Rusa <alex@rusa.at>
2025-05-25 14:17:32 +0000

    Fix decidim version requirement

--- lib/decidim/feeds/version.rb
+++ lib/decidim/feeds/version.rb
@@ -4,6 +4,6 @@ module Decidim
   # This holds the decidim-meetings version.
   module Feeds
     VERSION = "1.0.0"
-    COMPAT_DECIDIM_VERSION = [">= 0.28.0", "< 0.31"].freeze
+    COMPAT_DECIDIM_VERSION = [">= 0.30.0", "< 0.31"].freeze
   end
 end
