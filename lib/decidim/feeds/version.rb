dfdfb7abd279a3d374f05ca996d92741b64bfd89
Alexander Rusa <alex@rusa.at>
2025-05-25 17:52:32 +0200

    Release 3.0.0

--- lib/decidim/feeds/version.rb
+++ lib/decidim/feeds/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-meetings version.
   module Feeds
-    VERSION = "1.0.0"
+    VERSION = "3.0.0"
     COMPAT_DECIDIM_VERSION = [">= 0.30.0", "< 0.31"].freeze
   end
 end
